efb2c1701c23fbeea701be50172ab43ef628bf5c
Maria Elizarova <porosenie@gmail.com>
2018-10-13 15:07:28 +0300

    Add pet model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,51 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+  end
 
+  def create
+    # This action uses POST parameters. They are most likely coming
+    # from an HTML form which the user has submitted. The URL for
+    # this RESTful request will be "/clients", and the data will be
+    # sent as part of the request body.
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      # This line overrides the default rendering behavior, which
+      # would have been to render the "create" view.
+      render "new"
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    redirect_to posts_path if @post.destroy
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:weight, :description, :photo)
   end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_07_124256) do
+ActiveRecord::Schema.define(version: 2018_10_11_153548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pets", force: :cascade do |t|
+    t.string "name"
+    t.date "birth"
+    t.string "avatar"
+    t.string "animal"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "photo"
