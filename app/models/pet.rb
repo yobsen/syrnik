b2fc5db546ae399a66f2d8ba14109ffd3724115e
Maria Elizarova <porosenie@gmail.com>
2018-10-14 19:06:14 +0300

    Add basic validations to Pet

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,5 @@
 class Pet < ApplicationRecord
   mount_uploader :avatar, AvatarUploader
+  validates :age, numericality: { greater_than_or_equal_to: 0 }
+  validates :animal, :avatar, presence: true
 end
