a6580cd79b1a2dd8b3100a851b7aec081e3e2201
Maria Elizarova <porosenie@gmail.com>
2018-10-22 16:58:13 +0300

    Make a basic User model (including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  before_save { self.email = email.downcase }
+
+  validates :name,  presence: true, length: { maximum: 20 }
+  validates :email, presence: true, length: { maximum: 250 }
+                    format: { with: VALID_EMAIL_REGEX }
+                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+
+  has_secure_password
 end
